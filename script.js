6a1a75817a67e14a72bafa3557f0f355cc066fd1
juanfishp <122216565+juanfishp@users.noreply.github.com>
2024-06-08 11:57:30 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const second = 1000,
+          minute = second * 60,
+          hour = minute * 60,
+          day = hour * 24;
+  
+    const birthday = new Date("June 10, 2024 00:00:00").getTime();
+  
+    const countDown = setInterval(function () {
+      const now = new Date().getTime();
+      const distance = birthday - now;
+  
+      const daysRemaining = Math.floor(distance / day);
+      const hoursRemaining = Math.floor((distance % day) / hour);
+      const minutesRemaining = Math.floor((distance % hour) / minute);
+      const secondsRemaining = Math.floor((distance % minute) / second);
+  
+      // Actualizar los elementos <span> dentro de cada <li>
+      document.querySelector("#days span").textContent = daysRemaining;
+      document.querySelector("#hours span").textContent = hoursRemaining;
+      document.querySelector("#minutes span").textContent = minutesRemaining;
+      document.querySelector("#seconds span").textContent = secondsRemaining;
+  
+      if (distance < 0) {
+        document.getElementById("headline").innerText = "¡Feliz cumpleaños!";
+        document.getElementById("countdown").style.display = "none";
+        document.getElementById("content").style.display = "block";
+        clearInterval(countDown);
+      }
+    }, 1000);
+  });
